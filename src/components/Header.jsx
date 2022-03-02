0532a3ea4136dff64c9338663e9d6785c41cd686
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-03-02 19:24:17 -0300

    estruturando o projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from '../styles/Layout/Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={style.Container}>
+      <h1>Disney Personagens</h1>
+    </header>
+  )
+}
+
+export default Header
