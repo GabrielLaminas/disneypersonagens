622a1a87e32e99f0a8dbbe52c6c91bd7fea0463e
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-03-11 12:23:54 -0300

    ajustando style e a paginação

--- src/components/Helper/Pagination.jsx
+++ src/components/Helper/Pagination.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import style from '../../styles/components/Pagination.module.scss';
 
 const Pagination = ({ totalPage, page, setPage}) => {
-  const ArrayTotalPages = Array.from(Array(totalPage), (_, i) => i);
-  const itemsPerPage = 5;
 
-  function pagination(currentPage){
-    const startIndex = ArrayTotalPages[Number(currentPage)];
+  function pagination(totalPage, currentPage){
+    const arrayTotalPages = Array.from(Array(totalPage), (_, i) => i + 1);
+    const itemsPerPage = 7;
+    const startIndex = arrayTotalPages[currentPage - 1];
     const endIndex = startIndex + itemsPerPage;
-    let currentItems = ArrayTotalPages.slice(startIndex, endIndex);
-  
+    let currentItems = arrayTotalPages.slice(startIndex - 1, endIndex - 1);
+    
     if(startIndex !== 1){
-      currentItems = ArrayTotalPages.slice(startIndex-1, endIndex)
+      currentItems = arrayTotalPages.slice(startIndex - 2, endIndex - 2)
+      console.log('start ', startIndex - 2)
+      console.log('end ', endIndex - 2);
+      console.log(currentItems)
     }
-
     return currentItems;
   }
 
@@ -25,17 +27,21 @@ const Pagination = ({ totalPage, page, setPage}) => {
   }
 
   return (
-    <div className={style.container}>
-      {pagination(page).map((item, i) => (
-        <a 
+    <nav className={style.container}>
+      {/**<a href="#">&laquo;</a> */}
+      {pagination(totalPage, Number(page)).map((item, i) => (
+        <a
           key={i} 
-          className={style.page} 
+          className={
+            `${item === Number(page) ? style.active : style.page}`
+          } 
           onClick={handleClick}
         >
           {item}
         </a>
       ))}
-    </div>
+      {/*<a href="#">&raquo;</a> */}
+    </nav>
   )
 }
 
