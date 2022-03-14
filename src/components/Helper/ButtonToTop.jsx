0be0db6935949960f510a174a66db2cd2b2f806a
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-03-14 18:53:42 -0300

    finalizando buttonTop e ajustes finais

--- src/components/Helper/ButtonToTop.jsx
+++ src/components/Helper/ButtonToTop.jsx
@@ -3,10 +3,35 @@ import Arrow from '../../assets/arrowUp.svg?component';
 import style from '../../styles/components/ButtonToTop.module.scss';
 
 const ButtonToTop = () => {
+  const [visivel, setVisivel] = React.useState(false);
+  
+  React.useEffect(() => {
+
+    function handleScroll(){
+      if(window.scrollY >= 250){
+        setVisivel(true);
+      }
+      else{
+        setVisivel(false);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  function handleClick(event){
+    event.preventDefault();
+    window.scrollTo(0, 0);
+  }
 
   return (
     <a
       className={style.container}
+      style={{display: `${visivel ? 'flex' : 'none'}`}}
+      onClick={handleClick}
     >
       <Arrow />
     </a>
